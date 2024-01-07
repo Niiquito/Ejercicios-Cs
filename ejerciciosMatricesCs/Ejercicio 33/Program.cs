8fac3df89d843219e3b415d4e097c6e3bbb4d5d8
Niiquito Antunez <ffdfdfdd3@gmail.com>
2024-01-07 02:11:16 -0300

    Ejercicio Hecho

---
+++ ejerciciosMatricesCs/Ejercicio 33/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ejercicio33;
+public class Ejercicio33
+{
+    static void Main()
+    {
+        List<int> numeros = new List<int>();
+        int numero = 1;
+        Console.WriteLine("Ingrese numeros");
+        do
+        {
+            numero = int.Parse(Console.ReadLine());
+            numeros.Add(numero);
+        } while (numero !=0);
+        Console.WriteLine();
+        numeros.RemoveAt(numeros.Count - 1);
+        foreach (int elemento in numeros)
+        {
+            Console.WriteLine(elemento);
+        }
+    }
+}
