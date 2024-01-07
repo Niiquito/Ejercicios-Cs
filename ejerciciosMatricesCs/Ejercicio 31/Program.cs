a57aec3fc26a8c159b7d5773fad20833768506c2
Niiquito Antunez <ffdfdfdd3@gmail.com>
2024-01-07 00:39:06 -0300

    Sin ejercicio

---
+++ ejerciciosMatricesCs/Ejercicio 31/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ejercicio31;
+public class Ejercicio31
+{
+    static void Main()
+    {
+
+    }
+    private static void asignacionRandom(int[,] matriz, Random numeroRandom)
+    {
+        for (int i = 0; i < matriz.GetLength(0); i++)
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                matriz[i, j] = numeroRandom.Next(1, 10);
+            }
+    }
+    private static void graficoMatriz(int[,] matriz)
+    {
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                Console.Write(matriz[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
