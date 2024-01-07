00893b39021e5f917e7da508f94b497f149aca64
Niiquito Antunez <ffdfdfdd3@gmail.com>
2024-01-07 01:23:31 -0300

    Ejercicio Hecho

--- ejerciciosMatricesCs/Ejercicio 31/Program.cs
+++ ejerciciosMatricesCs/Ejercicio 31/Program.cs
@@ -3,7 +3,26 @@ public class Ejercicio31
 {
     static void Main()
     {
-
+        Random numeroRandom = new Random();
+        int[,] array1 = new int[3, 3];
+        asignacionRandom(array1,numeroRandom);
+        graficoMatriz(array1);
+        for (int i = 0; i < array1.GetLength(0); i++)
+        {
+            for (int j = 0; j < array1.GetLength(1); j++)
+            {
+                if (i==j)
+                {
+                    array1[i, j] = 1;
+                }
+                else
+                {
+                    array1[i,j] = 0;
+                }
+            }
+        }
+        Console.WriteLine();
+        graficoMatriz(array1);
     }
     private static void asignacionRandom(int[,] matriz, Random numeroRandom)
     {
