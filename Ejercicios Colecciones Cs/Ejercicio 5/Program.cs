4453145cc21ef201e4c8a825d54a936cd9cd1a65
Niiquito Antunez <ffdfdfdd3@gmail.com>
2024-01-09 00:51:00 -0300

    Ejercicio Hecho

---
+++ Ejercicios Colecciones Cs/Ejercicio 5/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Ejercicio5;
+public class Ejercicio5
+{
+    /*Crear una lista de palabras y, a continuación, 
+      realizar las siguientes operaciones:
+            Mostrar la lista en consola.
+            Eliminar todas las palabras que sean menores de 5 caracteres.
+            Añadir una nueva palabra al principio de la lista.
+            Ordenar la lista alfabéticamente.
+    */
+    static void Main()
+    {
+        List<string> palabras = new List<string>();
+        palabras.Add("Holas");
+        palabras.Add("Petes");
+        palabras.Add("Pito");
+        string palabra = "";
+        foreach (string item in palabras)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine();
+        for (int i = 0; i<palabras.Count; i++)
+        {
+            palabra = palabras[i];
+            if (palabra.Length < 5)
+            {
+                palabras.RemoveAt(i);
+                i--;
+            }
+        }
+        foreach(string item in palabras)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
