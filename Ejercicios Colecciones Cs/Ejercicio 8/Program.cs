fc97422a9d723d9be43ec3c9d150144772630eea
Niiquito Antunez <ffdfdfdd3@gmail.com>
2024-01-10 01:39:06 -0300

    Ejercicio Hecho

---
+++ Ejercicios Colecciones Cs/Ejercicio 8/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ejercicio8;
+public class Ejercicio8
+{
+    /*
+        Crear una lista de números enteros y, a continuación, 
+        realizar las siguientes operaciones:
+
+            Mostrar la lista en consola.
+            Eliminar todas las repeticiones de la lista.
+    */
+    static void Main()
+    {
+        List<int> numeros = new List<int>();
+        numeros.Add(10);
+        numeros.Add(10);
+        numeros.Add(30);
+        numeros.Add(20);
+        numeros.Add(30);
+        numeros.Add(40);
+        foreach (int item in numeros)
+            Console.WriteLine(item);
+        numeros.Sort();
+        Console.WriteLine();
+        for (int i = 0; i < numeros.Count - 1; i++)
+            if (numeros[i] == numeros[i+1] )
+                numeros.RemoveAt(i);
+        foreach (int item in numeros)
+            Console.WriteLine(item);
+    }
+}
