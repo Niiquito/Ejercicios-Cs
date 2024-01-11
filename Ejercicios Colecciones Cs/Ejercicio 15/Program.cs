eb45c94c88da9d705a09a701ea187213d649a723
Niiquito Antunez <ffdfdfdd3@gmail.com>
2024-01-11 02:03:39 -0300

    Ejercicio Hecho

---
+++ Ejercicios Colecciones Cs/Ejercicio 15/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Ejercicio15;
+public class Ejercicio15
+{
+    static void Main()
+    {
+        Queue<string> cola = new Queue<string>();
+        LinkedList<string> lista = new LinkedList<string>();
+        string palabra = "a";
+        palabra = ingresarPalabras(lista);
+        Console.WriteLine();
+        lista.RemoveLast();
+        Console.WriteLine("Mostrar elementos \n");
+        mostrarLista(lista);
+        Console.WriteLine("\nMostrar elementos en cola");
+        ponerElementosEnCola(cola, lista);
+        Console.WriteLine();
+        mostrarCola(cola);
+        Console.WriteLine($"\nPrimer elemento: {cola.Peek()}");
+        Console.WriteLine("\nCola eliminada con Enqueue");
+        quitarElementosDeCola(cola);
+        mostrarCola(cola);
+    }
+
+    private static string ingresarPalabras(LinkedList<string> lista)
+    {
+        string palabra;
+        do
+        {
+            palabra = Console.ReadLine();
+            lista.AddLast(palabra);
+        } while (palabra != "");
+        return palabra;
+    }
+
+    private static void mostrarLista(LinkedList<string> lista)
+    {
+        foreach (string item in lista)
+            Console.WriteLine(item);
+    }
+
+    private static void quitarElementosDeCola(Queue<string> cola)
+    {
+        int tamañoCola = cola.Count;
+        for (int i = 0; i < tamañoCola; i++)
+            cola.Dequeue();
+    }
+
+    private static void ponerElementosEnCola(Queue<string> cola, LinkedList<string> lista)
+    {
+        foreach (string item in lista)
+            cola.Enqueue(item);
+    }
+
+    private static void mostrarCola(Queue<string> cola)
+    {
+        foreach (string item in cola)
+            Console.WriteLine(item);
+    }
+}
