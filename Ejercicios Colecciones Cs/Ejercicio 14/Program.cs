024b641fdf9da39e83bfcf8dce58efb75bc8c36a
Niiquito Antunez <ffdfdfdd3@gmail.com>
2024-01-13 00:39:30 -0300

    Diccionario Hecho

---
+++ Ejercicios Colecciones Cs/Ejercicio 14/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ejercicio14;
+public class Ejercicio14
+{
+    static void Main()
+    {
+        Dictionary<string, int> persona = new Dictionary<string, int>();
+        for (int i = 0; i < 3; i++)
+        {
+            persona.Add(Console.ReadLine(), int.Parse(Console.ReadLine()));
+        }
+        Console.WriteLine();
+        foreach (KeyValuePair<string, int> informacion in persona)
+        {
+            Console.WriteLine($"Nombre: {informacion.Key}, Edad: {informacion.Value}");
+        }
+    }
+}
