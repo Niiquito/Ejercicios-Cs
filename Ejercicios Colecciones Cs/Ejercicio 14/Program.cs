44de0d535bb298020ab7a46ada5dea0177cb3830
Niiquito Antunez <ffdfdfdd3@gmail.com>
2024-01-15 03:51:17 -0300

    Modificación menor

--- Ejercicios Colecciones Cs/Ejercicio 14/Program.cs
+++ Ejercicios Colecciones Cs/Ejercicio 14/Program.cs
@@ -4,11 +4,21 @@ public class Ejercicio14
     static void Main()
     {
         Dictionary<string, int> persona = new Dictionary<string, int>();
-        for (int i = 0; i < 3; i++)
+        bool continuar = false;
+        do
         {
-            persona.Add(Console.ReadLine(), int.Parse(Console.ReadLine()));
-        }
+            try
+            {
+                persona.Add(Console.ReadLine(), int.Parse(Console.ReadLine()));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error, ingrese nuevamente", e);
+            }
+        } while (!(persona.ContainsKey("") && persona.ContainsValue(0)));
         Console.WriteLine();
+        int cero = 0;
+        persona.Remove<string, int>("", out cero);
         foreach (KeyValuePair<string, int> informacion in persona)
         {
             Console.WriteLine($"Nombre: {informacion.Key}, Edad: {informacion.Value}");
