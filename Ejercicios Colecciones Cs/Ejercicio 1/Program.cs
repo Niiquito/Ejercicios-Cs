ec425a320c8975bb3ca944cb8cc25a8075502890
Niiquito Antunez <ffdfdfdd3@gmail.com>
2024-01-26 05:08:28 -0300

    Modificación innecesariamente necesaria

--- Ejercicios Colecciones Cs/Ejercicio 1/Program.cs
+++ Ejercicios Colecciones Cs/Ejercicio 1/Program.cs
@@ -4,10 +4,21 @@ public class Ejercicio1
     static void Main()
     {
         LinkedList<int> numeros = new LinkedList<int>();
-        foreach (int numero in new int[] {1,2,3,4})
-            numeros.AddFirst(numero);
+        do
+        {
+            try
+            {
+                numeros.AddFirst(int.Parse(Console.ReadLine()));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Elemento inválido, ingrese nuevamente");
+            }
+        } while (!numeros.Contains(0));
+        numeros.Remove(0);
+        Console.WriteLine();
         foreach (int numero in numeros)
-            Console.WriteLine(numero);
+            Console.Write(numero + " ");
         Console.WriteLine();
     }
 }
