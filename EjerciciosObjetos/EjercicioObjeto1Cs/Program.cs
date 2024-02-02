2e12d87fd84745256bd8430cb727197c31cdb342
Niiquito Antunez <ffdfdfdd3@gmail.com>
2024-02-02 07:06:16 -0300

    Hecho

---
+++ EjerciciosObjetos/EjercicioObjeto1Cs/Program.cs
@@ -0,0 +1,37 @@
+﻿using LibreriaObjetos1;
+
+static class Ejercicio1
+{
+    static void Main()
+    {
+        Customer persona = new Customer();
+        List<string> nombre = new List<string>();
+        List<int> dni = new List<int>();
+        List<DateTime> fechaNacimiento = new List<DateTime>();
+        Console.WriteLine("Ingrese nombre, DNI y fecha de nacimiento");
+        do
+        {
+            Console.Write("Nombre: ");
+            persona.Name = Console.ReadLine();
+            if (persona.Name == "")
+                break;
+            else
+            {
+                Console.Write("DNI:");
+                persona.dni = int.Parse(Console.ReadLine());
+                Console.Write("Fecha de nacimiento: ");
+                persona.BirthDay = DateTime.Parse(Console.ReadLine());
+                nombre.Add(persona.Name);
+                dni.Add(persona.dni);
+                fechaNacimiento.Add(persona.BirthDay);
+            }
+        } while (persona.Name != "");
+        Console.ReadKey();
+        Console.Clear();
+
+        for (int i = 0; i < nombre.Count; i++)
+            Console.Write($"Nombre: {nombre[i]}\n" +
+                $"DNI: {dni[i]}\n" +
+                $"Fecha Nacimiento: {fechaNacimiento[i]}\n");
+    }
+}
---
+++ EjerciciosObjetos/LibreriaObjetos1/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace LibreriaObjetos1
+{
+    public class Customer
+    {
+        private string _name;
+        private int _dni;
+        private DateTime _birthDay;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int dni
+        {
+            get { return _dni;}
+            set { _dni = value; }
+        }
+        public DateTime BirthDay
+        {
+            get { return _birthDay;}
+            set { _birthDay = value; }
+        }
+    }
+}
