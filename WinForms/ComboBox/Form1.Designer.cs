908b8ea957960033599cea86147056df09e2896f
Niiquito Antunez <ffdfdfdd3@gmail.com>
2024-02-26 22:35:51 -0300

    Nuevo comboBox

---
+++ WinForms/ComboBox/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbx_genre = new System.Windows.Forms.ComboBox();
+            this.lbl_result = new System.Windows.Forms.Label();
+            this.btn_accept = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbx_genre
+            // 
+            this.cbx_genre.FormattingEnabled = true;
+            this.cbx_genre.Items.AddRange(new object[] {
+            "Hombre",
+            "Mujer"});
+            this.cbx_genre.Location = new System.Drawing.Point(194, 101);
+            this.cbx_genre.Name = "cbx_genre";
+            this.cbx_genre.Size = new System.Drawing.Size(121, 21);
+            this.cbx_genre.TabIndex = 0;
+            // 
+            // lbl_result
+            // 
+            this.lbl_result.AutoSize = true;
+            this.lbl_result.Location = new System.Drawing.Point(337, 167);
+            this.lbl_result.Name = "lbl_result";
+            this.lbl_result.Size = new System.Drawing.Size(35, 13);
+            this.lbl_result.TabIndex = 1;
+            this.lbl_result.Text = "label1";
+            // 
+            // btn_accept
+            // 
+            this.btn_accept.Location = new System.Drawing.Point(315, 203);
+            this.btn_accept.Name = "btn_accept";
+            this.btn_accept.Size = new System.Drawing.Size(75, 23);
+            this.btn_accept.TabIndex = 2;
+            this.btn_accept.Text = "Aceptar";
+            this.btn_accept.UseVisualStyleBackColor = true;
+            this.btn_accept.Click += new System.EventHandler(this.btn_accept_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_accept);
+            this.Controls.Add(this.lbl_result);
+            this.Controls.Add(this.cbx_genre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbx_genre;
+        private System.Windows.Forms.Label lbl_result;
+        private System.Windows.Forms.Button btn_accept;
+    }
+}
+
---
+++ WinForms/ComboBox/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_accept_Click(object sender, EventArgs e)
+        {
+            lbl_result.Text = cbx_genre.Text;
+        }
+    }
+}
