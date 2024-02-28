67a4fa65366a76181e0591604aebc9632c110a3d
Niiquito Antunez <ffdfdfdd3@gmail.com>
2024-02-28 20:52:15 -0300

    Mejor optimizado

--- WinForms/GeneroPais/Form1.cs
+++ WinForms/GeneroPais/Form1.cs
@@ -19,22 +19,36 @@ namespace GeneroPais
 
         private void btn_accept_Click(object sender, EventArgs e)
         {
-            if (rdbtn_male.Checked == true && rdbtn_country1.Checked == true)
-                lbl_result.Text = "Es hombre, de Argentina";
-            else if (rdbtn_male.Checked == true && rdbtn_country2.Checked == true)
-                lbl_result.Text = "Es hombre, de Brasil";
-            else if (rdbtn_male.Checked == true && rdbtn_country3.Checked == true)
-                lbl_result.Text = "Es hombre, de Uruguay";
-            else if (rdbtn_male.Checked == true && rdbtn_country4.Checked == true)
-                lbl_result.Text = "Es hombre, de Chile";
-            else if (rdbtn_female.Checked == true && rdbtn_country1.Checked == true)
-                lbl_result.Text = "Es mujer, de Argentina";
-            else if (rdbtn_female.Checked == true && rdbtn_country2.Checked == true)
-                lbl_result.Text = "Es mujer, de Brasil";
-            else if (rdbtn_female.Checked == true && rdbtn_country3.Checked == true)
-                lbl_result.Text = "Es mujer, de Uruguay";
-            else if (rdbtn_female.Checked == true && rdbtn_country4.Checked == true)
-                lbl_result.Text = "Es mujer, de Chile";
+            //if (rdbtn_male.Checked == true && rdbtn_country1.Checked == true)
+            //    lbl_result.Text = "Es hombre, de " + rdbtn_country1;
+            //else if (rdbtn_male.Checked == true && rdbtn_country2.Checked == true)
+            //    lbl_result.Text = "Es hombre, de Brasil";
+            //else if (rdbtn_male.Checked == true && rdbtn_country3.Checked == true)
+            //    lbl_result.Text = "Es hombre, de Uruguay";
+            //else if (rdbtn_male.Checked == true && rdbtn_country4.Checked == true)
+            //    lbl_result.Text = "Es hombre, de Chile";
+            //else if (rdbtn_female.Checked == true && rdbtn_country1.Checked == true)
+            //    lbl_result.Text = "Es mujer, de Argentina";
+            //else if (rdbtn_female.Checked == true && rdbtn_country2.Checked == true)
+            //    lbl_result.Text = "Es mujer, de Brasil";
+            //else if (rdbtn_female.Checked == true && rdbtn_country3.Checked == true)
+            //    lbl_result.Text = "Es mujer, de Uruguay";
+            //else if (rdbtn_female.Checked == true && rdbtn_country4.Checked == true)
+            //    lbl_result.Text = "Es mujer, de Chile";
+            string pepe = "";
+            if (rdbtn_male.Checked)
+                pepe = "Es " + rdbtn_male.Text;
+            else
+                pepe = "Es " + rdbtn_female.Text;
+            if (rdbtn_country1.Checked)
+                pepe += " de " + rdbtn_country1.Text;
+            else if (rdbtn_country2.Checked)
+                pepe += " de " + rdbtn_country2.Text;
+            else if (rdbtn_country3.Checked)
+                pepe += " de " + rdbtn_country3.Text;
+            else if (rdbtn_country4.Checked)
+                pepe += " de " + rdbtn_country4.Text;
+            lbl_result.Text = pepe;
         }
     }
 }
